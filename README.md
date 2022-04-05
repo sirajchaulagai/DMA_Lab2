c290b395226f09f5518a73f7ab65c0c27b4acced
sirajchaulagai <64361176+sirajchaulagai@users.noreply.github.com>
2022-04-05 09:31:51 +0545

    upadte

---
+++ README.md
@@ -0,0 +1,12 @@
+##heading
+
+###screenshot name
+
+![img](Screenshot_20220405_092533.png)
+![img](Screenshot_20220405_092600.png)
+![img](Screenshot_20220405_092613.png)
+![img](Screenshot_20220405_092626.png)
+![img](device-2022-04-05-084702.gif)
+
+
+**describe the screenshot**
--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mCount = 0;
+    private int mCount;
+    private Button btn_1,btn_2;
+
+    {
+        mCount = 0;
+    }
     private TextView mShowCount;
 
     @Override
@@ -17,16 +23,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        btn_1= findViewById(R.id.button_zero);
+        btn_2=findViewById(R.id.button_count);
     }
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
-        toast.show();
-    }
+    public void showToast(View view){
+        Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT).show();
 
-    public void countUp(View view) {
+    }
+    public void countUp(View view){
+
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        btn_1.setBackgroundResource(R.color.pink);
+        btn_2.setBackgroundResource(R.color.green);
+
     }
+
+    public void Zero(View view) {
+        mCount=0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        btn_1.setBackgroundResource(R.color.grey);
+
+    }
+
+
 }
